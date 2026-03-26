5f1a0979090925f2a4b5a0c1f17e06866c65577b
Xuanwei <hinwai.cheung@outlook.com>
2026-03-27 01:47:55 +0800

    test auto update

--- dags/example_dag.py
+++ dags/example_dag.py
@@ -19,16 +19,6 @@ with DAG(
         print("Goodbye world, from Airflow!")
 
 
-    @task
-    def sleep_for_a_while():
-        time.sleep(10)
-        print("Slept for a while!")
 
-    @task
-    def add_numbers(x: int, y: int) -> int:
-        time.sleep(5)
-        result = x + y
-        print(f"The sum of {x} and {y} is {result}")
-        return result
     
     hello_world() >> goodbye_world()
